7ab076f7552130b44c41f8c3dc44bc4dff2d6a63
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-26 22:13:15 +0330

    style: some change in style code

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,10 +3,10 @@ import InfoBox from "./InfoBox"
 
 const Footer = () => {
     return (
-        <div className="w-full bg-white fixed bottom-0 right-0 left-0 p-4 flex items-center justify-between gap-4 md:hidden shadow-black shadow-2xl">
-            <InfoBox />
-            <span className="w-1 bg-gray-400 h-24"></span>
-            <InfoBox />
+        <div className="w-full bg-white fixed bottom-0 right-0 left-0 p-4 flex items-center justify-between gap-4 md:hidden shadow-black shadow-2xl select-none">
+            <InfoBox title={"Round"} />
+            <span className="w-[2.5px] bg-gray-400 h-24"></span>
+            <InfoBox title={"Goal"} />
         </div>
     )
 }
--- src/app/components/InfoBox.tsx
+++ src/app/components/InfoBox.tsx
@@ -1,8 +1,12 @@
 
-const InfoBox = () => {
+type InfoBoxPropsType = {
+    title: string
+}
+
+const InfoBox = ({ title }: InfoBoxPropsType) => {
     return (
-        <div className="h-28 w-full flex items-center justify-center flex-col">
-            <h3 className="text-gray-400 font-semibold text-lg">Round</h3>
+        <div className="h-28 w-full flex items-center justify-center flex-col gap-2">
+            <h3 className="text-gray-400 font-semibold text-lg">{title}</h3>
             <h3 className="text-lg text-zinc-800"><span className="text-5xl text-zinc-800 font-medium">3</span>/4</h3>
         </div>
     )
