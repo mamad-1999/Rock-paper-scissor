0cdb526213ca178da6ee060c2bdf90bd0bd13eac
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-26 18:19:15 +0330

    fit: create NavBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
-    <main className="flex w-full min-h-screen">
-
+    <main className="flex w-full min-h-screen bg-indigo-400">
+      <NavBar />
     </main>
   )
 }
