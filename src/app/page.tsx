15897d7a21862fdee17165d38476d98d8f5e9877
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-28 18:23:14 +0330

    fit: change project format to next js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,8 @@
-import Footer from "./components/Footer";
-import NavBar from "./components/NavBar";
-import Time from "./components/Time";
 
 export default function Home() {
   return (
     <main className="w-full min-h-screen bg-indigo-400">
-      <NavBar />
-      <Time />
-      <Footer />
+
     </main>
   )
 }
