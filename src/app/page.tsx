fa476c3742f1ec8122f9332cb21b44a085f31078
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-05 18:22:02 +0330

    style: change color (home)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="w-full flex flex-col gap-8 items-center justify-center min-h-screen bg-stone-200">
       <Image src={"/images/banner.png"} width={300} height={300} alt={"banner"} />
-      <Link href={"/game"} className="text-2xl font-light uppercase underline underline-offset-8">
+      <Link href={"/game"} className="text-2xl font-light uppercase underline underline-offset-8 text-yellow-900">
         click me to start
       </Link>
     </main>
