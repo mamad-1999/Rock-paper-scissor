563a8c0bad0579df8b1a0040d138af450d44666a
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-05 18:19:43 +0330

    fit: create home {welcome banner} page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="w-full min-h-screen bg-indigo-400">
-
+    <main className="w-full flex flex-col gap-8 items-center justify-center min-h-screen bg-stone-200">
+      <Image src={"/images/banner.png"} width={300} height={300} alt={"banner"} />
+      <Link href={"/game"} className="text-2xl font-light uppercase underline underline-offset-8">
+        click me to start
+      </Link>
     </main>
   )
 }
