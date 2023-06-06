2a88aa2499b523e76c94417978355496ce13262a
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-06 19:21:47 +0330

    fit: add reducer in gameContext

--- src/app/context/gameContext.tsx
+++ src/app/context/gameContext.tsx
@@ -1,11 +1,18 @@
 "use client"
 
+import { createContext, useReducer, Dispatch } from "react";
+
 type StateType = {
     userScore: number
     pcScore: number
     gameTies: number
     roundCounter: number
     userSelect: string
+}
+
+type ActionType = {
+    type: string
+    payload: string
 }
 
 const initialState = {
@@ -16,13 +23,38 @@ const initialState = {
     userSelect: ""
 } as StateType
 
-import { createContext } from "react";
+const reducer = (state: StateType, action: ActionType) => {
+    switch (action.type) {
+        case "INCREMENT_USER_SCORE": {
+            return { ...state, userScore: state.userScore + 1 }
+        }
+        case "INCREMENT_PC_SCORE": {
+            return { ...state, psScore: state.pcScore + 1 }
+        }
+        case "INCREMENT_GAME_TIES": {
+            return { ...state, gameTies: state.gameTies + 1 }
+        }
+        case "INCREMENT_ROUND": {
+            return { ...state, roundCounter: state.roundCounter + 1 }
+        }
+        case "INCREMENT_ROUND": {
+            return { ...state, userSelect: action.payload }
+        }
+        default:
+            return state;
+    }
+}
 
-export const GameContext = createContext({})
+
+export const GameContext = createContext<{
+    state: StateType;
+    dispatch: Dispatch<ActionType>;
+}>({ state: initialState, dispatch: () => null });
 
 const GameContextProvider = ({ children }: { children: React.ReactNode }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
     return (
-        <GameContext.Provider value={{}}>
+        <GameContext.Provider value={{ state, dispatch }}>
             {children}
         </GameContext.Provider>
     )
