2e918083fcc13d8ac29395b0a6dcb26537a5ca95
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-06 18:50:01 +0330

    style: change some style in responsive sections

--- src/app/components/Scores.tsx
+++ src/app/components/Scores.tsx
@@ -4,7 +4,7 @@ import ScoreBox from "./ScoreBox"
 
 const Scores = () => {
     return (
-        <div className="w-full flex py-6 px-8 justify-center items-center gap-12">
+        <div className="w-full flex py-6 px-8 justify-center items-center gap-12 md:gap-28">
             <ScoreBox title="Win" score={0} />
             <ScoreBox title="Equal" score={0} />
             <ScoreBox title="Win" score={0} />
