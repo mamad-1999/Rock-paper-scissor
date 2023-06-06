4566353e6758a063413c13d321136762f6583f00
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-06 11:32:46 +0330

    fit: create Round component

---
+++ src/app/components/Round.tsx
@@ -0,0 +1,11 @@
+
+const Round = () => {
+    return (
+        <div className="w-full flex items-center justify-center flex-col gap-2 py-3">
+            <span className="text-xl">Round</span>
+            <span className="text-4xl">0</span>
+        </div>
+    )
+}
+
+export default Round
--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,11 @@
+import Round from "../components/Round"
 import Scores from "../components/Scores"
 
 const GamePage = () => {
     return (
         <div className="w-full min-h-screen bg-stone-200 flex flex-col">
             <Scores />
+            <Round />
         </div>
     )
 }
