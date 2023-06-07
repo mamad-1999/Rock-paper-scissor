a35e7197e54b650a9820056341ee885718674141
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-07 11:36:12 +0330

    fit: handle Scores color

--- src/app/components/ScoreBox.tsx
+++ src/app/components/ScoreBox.tsx
@@ -1,14 +1,15 @@
 type ScoreBoxPropsType = {
     title: string
     score: number | string
+    scoreColor: string
 }
 
 
-const ScoreBox = ({ score, title }: ScoreBoxPropsType) => {
+const ScoreBox = ({ score, title, scoreColor }: ScoreBoxPropsType) => {
     return (
         <div className="flex flex-col justify-center items-center gap-3">
             <span className="text-xl bg-zinc-800 text-white rounded-lg py-1 px-3">{title}</span>
-            <span className="text-4xl font-medium">{score}</span>
+            <span className={`text-5xl ${scoreColor} font-medium`}>{score}</span>
         </div>
     )
 }
--- src/app/components/Scores.tsx
+++ src/app/components/Scores.tsx
@@ -8,9 +8,26 @@ const Scores = () => {
     const { gameTies, pcScore, userScore } = state
     return (
         <div className="w-full flex py-6 px-8 justify-center items-center gap-12 md:gap-28">
-            <ScoreBox title="Win" score={userScore} />
-            <ScoreBox title="Equal" score={gameTies} />
-            <ScoreBox title="Win" score={pcScore} />
+            <ScoreBox
+                title="Win"
+                score={userScore}
+                scoreColor={userScore > pcScore ?
+                    "text-green-500" :
+                    userScore === pcScore ?
+                        "text-blue-500" :
+                        "text-red-500"} />
+            <ScoreBox
+                title="Equal"
+                score={gameTies}
+                scoreColor="text-zinc-900" />
+            <ScoreBox
+                title="Win"
+                score={pcScore}
+                scoreColor={pcScore > userScore ?
+                    "text-green-500" :
+                    userScore === pcScore ?
+                        "text-blue-500" :
+                        "text-red-500"} />
         </div>
     )
 }
