5d6369e61d7d53eadfc3ae91bb96ebce1f235656
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-10 18:19:23 +0330

    fit: add new dark light color to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,12 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      backgroundColor: {
+        primary: 'var(--color-bg-primary)',
+      },
+      textColor: {
+        primary: 'var(--text-color)',
+      },
     },
   },
   plugins: [],
