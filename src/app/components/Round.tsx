51d0181b86a70f890c082100549850514af71836
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-12 16:30:52 +0330

    style: change Round text size in lg mode

--- src/app/components/Round.tsx
+++ src/app/components/Round.tsx
@@ -7,7 +7,7 @@ const Round = ({ round }: RoundProps) => {
     return (
         <div className="w-full flex items-center justify-center flex-col gap-2 py-3">
             <span className="text-xl text-primary">Round</span>
-            <span className="text-6xl text-primary">{round}</span>
+            <span className="text-6xl lg:text-7xl text-primary">{round}</span>
         </div>
     )
 }
