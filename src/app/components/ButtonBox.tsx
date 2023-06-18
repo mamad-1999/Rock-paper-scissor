b2517cd98a25f2717e83255d17a6538836c42feb
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-18 10:39:14 +0330

    fit: add reset sound

--- src/app/components/ButtonBox.tsx
+++ src/app/components/ButtonBox.tsx
@@ -1,24 +1,32 @@
 "use client"
 
+import { Howl } from "howler"
 import { useGameContext } from "../context/gameContext"
 
 const ButtonBox = () => {
     const { dispatch } = useGameContext()
+
+    const sound = new Howl({
+        src: ["/resetSound.mp3"]
+    })
+
+    const resetGame = () => {
+        dispatch({ type: "RESET" })
+        sound.play()
+    }
+
     return (
         <div className="flex flex-col md:flex-row gap-2 md:gap-4 absolute md:left-8 md:top-8 top-40 left-8 z-10">
-            <div onClick={() => dispatch({ type: "RESET" })} className="flex justify-center items-center w-10 md:w-12 h-10 md:h-12 rounded-3xl bg-zinc-600 cursor-pointer">
+            <div onClick={resetGame} className="flex justify-center items-center w-10 md:w-12 h-10 md:h-12 rounded-3xl bg-zinc-600 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute w-6 md:w-8 h-6 md:h-8 stroke-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                 </svg>
-
             </div>
             <div className="flex justify-center items-center w-10 md:w-12 h-10 md:h-12 rounded-3xl bg-zinc-600 cursor-pointer">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute w-6 md:w-8 h-6 md:h-8 stroke-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z" />
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                 </svg>
-
-
             </div>
         </div>
     )
