c0af5b9b45b1344bc05144df9476408d6edf7078
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-06-27 18:00:47 +0330

    fit: add animation to Round

--- src/app/components/Round.tsx
+++ src/app/components/Round.tsx
@@ -1,12 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 type RoundProps = {
   round: number;
 };
 
 function Round({ round }: RoundProps) {
+  const [animation, setAnimation] = useState(false);
+
+  useEffect(() => {
+    setAnimation(true);
+    const timer = setTimeout(() => {
+      setAnimation(false);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [round]);
+
   return (
     <div className="w-full flex items-center justify-center flex-col gap-2 py-3">
       <span className="text-xl text-primary">Round</span>
-      <span className="text-6xl lg:text-7xl text-primary">{round}</span>
+      <span
+        className={`text-6xl lg:text-7xl text-primary ${
+          animation && "animate-ping"
+        }`}
+      >
+        {round}
+      </span>
     </div>
   );
 }
