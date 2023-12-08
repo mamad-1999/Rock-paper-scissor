8d2715cb352889736b220dd3081f8fc431655f38
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-12-08 10:21:09 +0330

    feat: create PWA app

--- next.config.js
+++ next.config.js
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
+
+// Configuration object tells the next-pwa plugin 
+const withPWA = require("next-pwa")({
+    dest: "public", // Destination directory for the PWA files
+    // disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+    register: true, // Register the PWA service worker
+    skipWaiting: true, // Skip waiting for service worker activation
+});
+
+// Export the combined configuration for Next.js with PWA support
+module.exports = withPWA(nextConfig);
