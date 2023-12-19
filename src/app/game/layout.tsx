85eabdfd242495c9bed8df9706503addb62c96be
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-12-19 16:58:27 +0330

    chore: remove unused code

--- src/app/game/layout.tsx
+++ src/app/game/layout.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ThemeProvider from "../context/ThemeContext";
 import GameContextProvider from "../context/gameContext";
-// import "../flip.css";
 
 export default function GameLayout({
   children,
